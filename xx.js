56db8f15e8f41e890668387215cf1645e18f86dd
danke778899 <usom818@gmail.com>
2024-06-14 23:27:48 +0800

    Update xx.js

--- xx.js
+++ xx.js
@@ -1,1 +1,28 @@
 
+
+document.writeln("<style>.imgtop{width:100%;}</style>");
+function ht_top() {
+document.writeln("<a href='https://www.yk1s477.vip:9077/user/register/?agent_code=50002327' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/xk1.gif'></a>");
+document.writeln("<a href='https://www.yk1s477.vip:9077/user/register/?agent_code=50002327' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/xk2.gif'></a>");
+document.writeln("<a href='https://www.yk1s477.vip:9077/user/register/?agent_code=50002327' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/xk3.gif'></a>");
+document.writeln("<a href='https://www.lejing2036.com:30479/entry/register/?i_code=8392365' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/by.gif'></a>");
+document.writeln("<a href='https://www.lejing2036.com:30479/entry/register/?i_code=8392365' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/by.gif'></a>");
+document.writeln("<a href='https://www.fbjd8c.com:9010/entry/register78836/?i_code=42410296' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/jy.gif'></a>");
+document.writeln("<a href='https://www.fbjd8c.com:9010/entry/register78836/?i_code=42410296' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/jy.gif'></a>");
+}
+
+
+function ht_pay() {
+document.writeln("<a href='https://www.yk1s477.vip:9077/user/register/?agent_code=50002327' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/xk.gif'></a>");
+document.writeln("<a href='https://www.lejing2036.com:30479/entry/register/?i_code=8392365' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/by.gif'></a>");
+document.writeln("<a href='https://www.fbjd8c.com:9010/entry/register78836/?i_code=42410296' target='_blank'><img class='imgtop' src='https://avxx-123.xyz/html/jy.gif'></a>");
+
+}
+
+function ht_foot() {
+
+
+
+}
+
+
